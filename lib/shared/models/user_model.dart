d272ba959ad915be086af3fa2e288d4f9c149373
Tushar Saini <sainitushar438@gmail.com>
2026-01-29 09:59:21 +0530

    test

--- lib/shared/models/user_model.dart
+++ lib/shared/models/user_model.dart
@@ -117,5 +117,4 @@ class UserModel {
   bool get isGuide => role == 'guide';
   bool get isAdmin => role == 'admin';
   bool get isTourist => role == 'tourist';
-  bool get isTest => role == "test";
 }
